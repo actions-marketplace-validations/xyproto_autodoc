12448d4729b96fbf52f96bee62d1b8c3b9a92196
Alexander F. Rødseth <rodseth@gmail.com>
2023-10-10 17:31:06 +0200

    Support comma separated globs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,12 @@
 # Variables
 MODEL=${INPUT_MODEL}
 TEMPERATURE=${INPUT_TEMPERATURE:-0.2}
+GLOBS=${INPUT_GLOBS:-*.go}  # Default to *.go if no globs are provided
 
 # Check if /workspace exists
 if [ ! -d "/workspace" ]; then
     echo "The /workspace directory needs to exist for the documentation generation to work."
-    exit 0 // exit normally
+    exit 0  # exit normally
 fi
 
 # Generate a Modelfile
@@ -15,8 +16,11 @@ echo "FROM $MODEL" > Modelfile
 echo "PARAMETER temperature $TEMPERATURE" >> Modelfile
 echo 'SYSTEM """' >> Modelfile
 
-# Append all Go files to the Modelfile
-find /workspace -name "*.go" -type f -exec cat {} + >> Modelfile
+# Append files matching the globs to the Modelfile
+IFS=',' read -ra GLOB_ARRAY <<< "$GLOBS"  # Convert comma-separated globs into an array
+for GLOB in "${GLOB_ARRAY[@]}"; do
+    find /workspace -name "$GLOB" -type f -exec cat {} + >> Modelfile
+done
 
 echo "Write the API documentation for the above code." >> Modelfile
 echo '"""' >> Modelfile
