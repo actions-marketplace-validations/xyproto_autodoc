33ca9e810d7759dfa275933b0b29e33c55f50ee4
Alexander F. Rødseth <rodseth@gmail.com>
2023-10-10 17:58:16 +0200

    Use GITHUB_WORKSPACE

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ MODEL=${INPUT_MODEL}
 TEMPERATURE=${INPUT_TEMPERATURE:-0.2}
 GLOBS=${INPUT_GLOBS:-*.go}  # Default to *.go if no globs are provided
 
-# Check if /workspace exists
-if [ ! -d "/workspace" ]; then
-    echo "The /workspace directory needs to exist for the documentation generation to work."
+# Check if GITHUB_WORKSPACE exists
+if [ ! -d "$GITHUB_WORKSPACE" ]; then
+    echo "The $GITHUB_WORKSPACE directory needs to exist for the documentation generation to work."
     exit 0  # exit normally
 fi
 
@@ -19,7 +19,7 @@ echo 'SYSTEM """' >> Modelfile
 # Append files matching the globs to the Modelfile
 IFS=',' read -ra GLOB_ARRAY <<< "$GLOBS"  # Convert comma-separated globs into an array
 for GLOB in "${GLOB_ARRAY[@]}"; do
-    find /workspace -name "$GLOB" -type f -exec cat {} + >> Modelfile
+    find "$GITHUB_WORKSPACE" -name "$GLOB" -type f -exec cat {} + >> Modelfile
 done
 
 echo "Write the API documentation for the above code." >> Modelfile
@@ -39,6 +39,6 @@ MODEL_NAME=$(echo "${MODEL}_custom" | tr ':-' '__')
 ollama create "${MODEL_NAME}" -f Modelfile
 
 # Run Ollama and display the output
-mkdir -p /workspace/github-pages
-ollama run "${MODEL_NAME}" "Use professional English. Generate a Markdown document." > "/workspace/github-pages/${MODEL_NAME}.md"
-cat "/workspace/github-pages/${MODEL_NAME}.md"
+mkdir -p "$GITHUB_WORKSPACE/github-pages"
+ollama run "${MODEL_NAME}" "Use professional English. Generate a Markdown document." > "$GITHUB_WORKSPACE/github-pages/${MODEL_NAME}.md"
+cat "$GITHUB_WORKSPACE/github-pages/${MODEL_NAME}.md"
